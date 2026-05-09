6600add383a7b5ae5a300dd309011b1a6ed086c6
Johnnyadee <johnadeoluwai@yahoo.com>
2026-05-09 19:38:52 +0100

    feat: refactor Chat component to group messages and improve scrolling behavior

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,18 +1,43 @@
+import { useRef, useEffect, useMemo } from "react";
 import Markdown from "react-markdown";
 import styles from "./Chat.module.css";
 
+const WELCOME_MESSAGE_GROUP = [{
+  role: "assistant",
+  content: "Hello how can I assist you right now?",
+}]
+;
+
 export default function Chat({ messages }) {
-  const WELCOME_MESSAGE = {
-    role: "assistant",
-    content: "Hello how can I assist you right now?",
-  };
+  const messagesEndRef = useRef(null);
+  const messagesGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+    [messages],
+  );
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className={styles.Chat}>
-      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-        <div key={index} data-role={role} className={styles.Message}>
-          <Markdown>{content}</Markdown>
+      {[WELCOME_MESSAGE_GROUP, ...messagesGroups].map((messages, groupIndex) => (
+        // group
+        <div key={groupIndex} className={styles.Group}>
+          {messages.map(({ role, content }, messageIndex) => (
+            // message
+            <div key={messageIndex} className={styles.Message} data-role={role}>
+              <Markdown>{content}</Markdown>
+            </div>
+          ))}
         </div>
       ))}
+
+      <div ref={messagesEndRef} />
     </div>
   );
 }
